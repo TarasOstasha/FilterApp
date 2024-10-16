cca7fefe9d38972adecbb9585a1ad5514b416d3a
taras ostasha <tonyjoss1990@gmail.com>
2024-10-16 17:06:37 -0400

    add admin page import export

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import './styles/App.scss';
+import CsvImportExport from './pages/Home/CsvImportExport/CsvImportExport';
 
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<CsvImportExport />} />
+        </Routes>
+      </Router>
+      {/* <Home /> */}
     </div>
   );
 };
