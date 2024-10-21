2325d2c3a7ae870d778ac6693417a2d8114c5377
taras ostasha <tonyjoss1990@gmail.com>
2024-10-21 10:30:25 -0400

    refactor import multer

--- server/routes/importRouter.js
+++ server/routes/importRouter.js
@@ -2,42 +2,48 @@ const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
 const { importController } = require('../controllers');
+const { upload } = require('../middleware');
 
 const importRouter = Router();
 
 // Multer configuration
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../services/import/data'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${file.originalname}`);
-    }
-});
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, path.join(__dirname, '../services/import/data'));
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, `${file.originalname}`);
+//     }
+// });
 
-// File filter for CSV files
-const fileFilter = (req, file, cb) => {
-    // Check the file extension or MIME type
-    if (file.mimetype === 'text/csv' || file.originalname.endsWith('.csv')) {
-        cb(null, true); // Accept the file
-    } else {
-        cb(new Error('Only .csv files are allowed!'), false); // Reject the file
-    }
-};
+// // File filter for CSV files
+// const fileFilter = (req, file, cb) => {
+//     // Check the file extension or MIME type
+//     if (file.mimetype === 'text/csv' || file.originalname.endsWith('.csv')) {
+//         cb(null, true); // Accept the file
+//     } else {
+//         cb(new Error('Only .csv files are allowed!'), false); // Reject the file
+//     }
+// };
 
-const upload = multer({
-    storage,
-    fileFilter,
-});
+// const upload = multer({
+//     storage,
+//     fileFilter,
+// });
 
 importRouter
-    .post('/category', upload.single('file'), importController.importCategories)
-    .post('/product', upload.single('file'), importController.importProducts)
-    .post('/product-category', upload.single('file'), importController.importProductCategories)
-    .post('/product-filter', upload.single('file'), importController.importProductFilters)
-    .post('/filter-field', upload.single('file'), importController.importFilterFields);
+    // .post('/category', upload.single('file'), importController.importCategories)
+    // .post('/product', upload.single('file'), importController.importProducts)
+    // .post('/product-category', upload.single('file'), importController.importProductCategories)
+    // .post('/product-filter', upload.single('file'), importController.importProductFilters)
+    // .post('/filter-field', upload.single('file'), importController.importFilterFields);
+    .post('/category', upload.uploadCsvData, importController.importCategories)
+    .post('/product', upload.uploadCsvData, importController.importProducts)
+    .post('/product-category', upload.uploadCsvData, importController.importProductCategories)
+    .post('/product-filter', upload.uploadCsvData, importController.importProductFilters)
+    .post('/filter-field', upload.uploadCsvData, importController.importFilterFields);
 
 module.exports = importRouter;
 
 
-
+// uploadUserPhoto 
