b46748229047572b5ede9aacd29aee2fbeb6b006
unknown <tonyjoss1990@gmail.com>
2025-08-13 15:42:47 -0400

    add reload admin img click

--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,5 +1,6 @@
 // src/pages/Admin/Admin.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import styles from './Admin.module.scss';
 import logo from './Logo-Master.png';
@@ -15,14 +16,16 @@ const Admin: React.FC = () => {
     return (
         <div className={styles['admin-wrapper']}>
             <div>
-                <img src={logo} alt="xyzdsplays" />
+                <Link to="/admin" reloadDocument>
+                    <img src={logo} alt="xyzdsplays" style={{ cursor: 'pointer' }} />
+                </Link>
             </div>
             <div className={styles['admin-panel']}>
                 {/* <h2>Admin Panel</h2> */}
                 <button onClick={handleLogout} className={styles['logout-button']}>
                     Logout
                 </button>
-      
+
             </div>
         </div>
     );
