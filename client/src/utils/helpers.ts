838310b6068b71e15d25d813a155ec5c2e8657ee
unknown <tonyjoss1990@gmail.com>
2025-12-24 09:56:21 -0500

    checking categories test

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -2,7 +2,7 @@ export const getCategoryIdFromPath = (): string => {
     try {
       const path = window.location.pathname; 
       const m = path.match(/\/(\d+)\.htm(?:$|\?)/i);
-      return m ? m[1] : '10709'; // Empty string = show all categories/products
+      return m ? m[1] : '747'; // Empty string = show all categories/products
     } catch {
       return ''; 
     }
